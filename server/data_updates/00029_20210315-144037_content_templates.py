65b279059d0b9033b8a8e7a034ae3f4aec17d3aa
Jérôme Poisson <jerome.poisson@sourcefabric.org>
2021-03-16 13:10:12 +0100

    [DATA UPDATA] migration script to fix templates linking to removed desks
    SDCP-488

---
+++ server/data_updates/00029_20210315-144037_content_templates.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8; -*-
+# This file is part of Superdesk.
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+#
+# Author  : Jérôme
+# Creation: 2021-03-15 14:40
+
+from superdesk.commands.data_updates import BaseDataUpdate
+from superdesk import get_resource_service
+
+
+class DataUpdate(BaseDataUpdate):
+
+    resource = "content_templates"
+    desks_field = "template_desks"
+
+    def forwards(self, mongodb_collection, mongodb_database):
+        """Check templates to remove references to deleted desks (SDCP-488)"""
+        vocabularies_service = get_resource_service(self.resource)
+        desks_service = get_resource_service("desks")
+        templates_collection = mongodb_database[self.resource]
+
+        for template in vocabularies_service.find({}):
+            desks = template.get(self.desks_field) or []
+            to_delete = []
+            for desk in desks:
+                found = desks_service.find_one(req=None, _id=desk)
+                if found is None:
+                    to_delete.append(desk)
+            if to_delete:
+                print(f"found deleted desk(s) in template {template.get('template_name')!r}")
+                desks = [d for d in desks if d not in to_delete]
+                print(
+                    templates_collection.update(
+                        {"_id": template["_id"]},
+                        {
+                            "$set": {self.desks_field: desks},
+                        },
+                    )
+                )
+                print(f"template {template.get('template_name')!r} fixed")
+
+
+    def backwards(self, mongodb_collection, mongodb_database):
+        pass
